9a6e8db0a732bdbd2164474488c8eccd32357e3b
Raman_Dhingra <Raman_Dhingra@wifi-roaming-128-4-59-177.host.udel.edu>
2017-02-08 13:53:24 -0500

    Updated although it has some errors that I haven't quite worked out yet

--- lab1/src/pkg/retirementcalc.java
+++ lab1/src/pkg/retirementcalc.java
@@ -1,5 +1,31 @@
 package pkg;
-
-public class retirementcalc {
+import java.util.Scanner;
+public class retirementcalc 
+{
+	public static void main(String[] args) 
+	{
+		Scanner input = new Scanner(System.in);
+		double yearstowork;
+		double annualreturn;
+		double yearsretired;
+		double reqincome;
+		double monthlyssi;
+		double whatyouneedsaved=1;
+		double saveeachmonth=1;
+		System.out.println("How many years do you have to work?");
+		yearstowork=input.nextDouble();
+		System.out.println("What is your annual return?");
+		annualreturn=input.nextDouble();
+		System.out.println("How many years until retirement?");
+		yearsretired=input.nextDouble();
+		System.out.println("What is your required income(per year)?");
+		reqincome=input.nextDouble();
+		System.out.println("what is your monthly SSI(expected Social Security) income?");
+		monthlyssi=input.nextDouble();
+		//whatyouneedsaved=some formula I can't find on the internet right now
+		//saveeachmonth= some formula I can't find on the internet right now
+		System.out.printf("You need to save $ %.2f throughout your entire career.",whatyouneedsaved);
+		System.out.printf("\nYou need to save $ %.2f from now on until retirement.",saveeachmonth);
+	}
 
 }
