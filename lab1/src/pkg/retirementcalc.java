fb7901a9ab5f2face9c8bc64b09cbf1114afba1e
Raman_Dhingra <Raman_Dhingra@wifi-roaming-128-4-61-137.host.udel.edu>
2017-02-15 12:57:57 -0500

    added split returns

--- lab1/src/pkg/retirementcalc.java
+++ lab1/src/pkg/retirementcalc.java
@@ -13,24 +13,27 @@ public class retirementcalc
 	{
 		Scanner input = new Scanner(System.in);
 		double yearstowork;
-		double annualreturn;
+		double annualreturnwork;
+		double annualreturnret;
 		double yearsretired;
 		double reqincome;
 		double monthlyssi;
 		System.out.println("How many years do you have to work?");
 		yearstowork=input.nextDouble();
-		System.out.println("What is your annual return?");
-		annualreturn=input.nextDouble();
+		System.out.println("What is your current annual return");
+		annualreturnwork=input.nextDouble();
+		System.out.println("What is your planned annual return at retirement?");
+		annualreturnret=input.nextDouble();
 		System.out.println("How many years are you planning for your retirment?");
 		yearsretired=input.nextDouble();
 		System.out.println("What is your required income(per year)?");
 		reqincome=input.nextDouble();
 		System.out.println("what is your monthly SSI(expected Social Security) income?");
 		monthlyssi=input.nextDouble();
-		whatyouneedtosave(yearstowork,annualreturn,reqincome,monthlyssi);
+		whatyouneedtosave(yearstowork,annualreturnwork,annualreturnret,reqincome,monthlyssi);
 	}
 
-	public static double whatyouneedtosave(double yearstowork,double annualreturn,double requincome,double monthlyssi){
+	public static double whatyouneedtosave(double yearstowork,double annualreturnwork,double annualreturnret,double requincome,double monthlyssi){
 		double whatyouneedsaved= 1;
 		saveeachmonth(whatyouneedsaved);
 		return whatyouneedsaved;
