6bce32bd1874792797a446c354673c45934e25dd
Andrew Alberding <Andrew Alberding@DESKTOP-VVL5H1O>
2017-02-21 19:07:32 -0500

    Found and implemented equations for annuity.

--- lab1/src/pkg/retirementcalc.java
+++ lab1/src/pkg/retirementcalc.java
@@ -1,6 +1,8 @@
 // CISC181 030L Lab Group 1
+//Andrew Alberding, Jason Anderson, Jonathan Barnes
 package pkg;
 import java.util.Scanner;
+import java.lang.Math;
 public class retirementcalc 
 {
 	public static void main(String[] args) 
@@ -12,13 +14,12 @@ public class retirementcalc
 		double yearsretired;
 		double reqincome;
 		double monthlyssi;
-		double whatyouneedsaved= 1;
-		double saveeachmonth=1;
+		
 		System.out.println("How many years do you have to work?");
 		yearstowork=input.nextDouble();
-		System.out.println("What is your current annual return");
+		System.out.println("What is your annual return for work in decimal form?");
 		annualreturnwork=input.nextDouble();
-		System.out.println("What is your planned annual return at retirement?");
+		System.out.println("What is your annual return at retirement in decimal form?");
 		annualreturnret=input.nextDouble();
 		System.out.println("How many years are you planning for your retirment?");
 		yearsretired=input.nextDouble();
@@ -26,9 +27,12 @@ public class retirementcalc
 		reqincome=input.nextDouble();
 		System.out.println("what is your monthly SSI(expected Social Security) income?");
 		monthlyssi=input.nextDouble();
-	
+		
+		//The following two equations are implementations of the annuity calculator formulas. 
+		double whatyouneedsaved = ((reqincome-monthlyssi)*12)*((1-(Math.pow(1+annualreturnret,-yearsretired)))/annualreturnret);
+		double saveeachmonth = whatyouneedsaved*((annualreturnwork/12)/(Math.pow(1+(annualreturnwork/12),yearstowork*12)-1));
 		System.out.printf("You need to save $ %.2f throughout your entire career.",whatyouneedsaved);
-		System.out.printf("\nYou need to save $ %.2f from now on until retirement.",saveeachmonth);
+		System.out.printf("\nYou need to save $ %.2f every month until retirement.",saveeachmonth);
 	}
 	
 }
